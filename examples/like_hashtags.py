04b089fce258a3021d0d98ed18ca1aa121cda97d
Naruto-23 <72311451+Naruto-23@users.noreply.github.com>
2020-10-04 23:27:49 +0900

    Update like_hashtags.py

--- examples/like_hashtags.py
+++ examples/like_hashtags.py
@@ -24,3 +24,14 @@ bot.login(username=args.u, password=args.p, proxy=args.proxy)
 
 for hashtag in args.hashtags:
     bot.like_hashtag(hashtag)
+    tags = ["ここに","いいねしたい","タグを","入力しておく"]
+wait = 25 * 60 # in seconds => 25 minutes
+retry = 5 * 60 * 60 # in hours => 5 hours
+
+while True:
+try:
+for hashtag in tags:
+bot.like_hashtag(hashtag)
+sleep(wait)
+except:
+sleep(retry)
